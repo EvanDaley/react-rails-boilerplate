fefc264884beb3c37b620a3ad21671676e79c11f
EvanDaley <daley.evan@gmail.com>
2021-08-28 18:23:17 -0700

    Small tweak

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HealthCheck from "./components/HealthCheck";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HealthCheck/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
